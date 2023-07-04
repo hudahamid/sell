ede437a830d7c74d8d28dea1465c999232416eae
hudahamid <huda.00091581@gmail.com>
2023-07-05 00:59:15 +0300

    angular js setups

---
+++ src/app/contact-page/contact-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-contact-page',
+  templateUrl: './contact-page.component.html',
+  styleUrls: ['./contact-page.component.css']
+})
+export class ContactPageComponent {
+
+}
